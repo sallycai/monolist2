86a2e96daecd066f61662b5a84127bf5060aad2c
sallycai979 <sallycai979@gmail.com>
2017-01-15 11:11:32 +0000

    Fix have and want view page

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -9,12 +9,17 @@ end
   def have
      @have_ranking = Have.group(:item_id).order('count_id desc').limit(10).count(:id)
      @item_ids = @have_ranking.keys
+     @items = Item.find(@item_ids).sort_by do |e|
+         @item_ids.index(e)
+     end
   end
 
   def want
-   @want_ranking = Want.group(:item_id).order('count_id desc').limit(10).count(:id)
-   @item_ids = @want_ranking.keys
-
- end
+     @want_ranking = Want.group(:item_id).order('count_id desc').limit(10).count(:id)
+     @item_ids = @want_ranking.keys
+     @items = Item.find(@item_ids).sort_by do |e|
+         @item_ids.index(e)
+     end
+  end
 end
 
