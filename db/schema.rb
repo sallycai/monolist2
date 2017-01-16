20fa7c97835d5ae753cf7227d9cfb67e51a93c2a
sallycai979 <sallycai979@gmail.com>
2017-01-16 06:52:37 +0000

    Data reset

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170109112633) do
-
-  create_table "haves", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20160426042549) do
 
   create_table "items", force: :cascade do |t|
     t.string   "title"
@@ -61,11 +56,6 @@ ActiveRecord::Schema.define(version: 20170109112633) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-  end
-
-  create_table "wants", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
